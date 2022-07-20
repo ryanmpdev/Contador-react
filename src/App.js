167515ae35ecbb0cbe0fffcd89ed2da012db1cb8
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-20 20:11:45 -0300

    Contador com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component{
 
-export default App;
+  state = {
+    numero: 0
+  }
+
+  add = () => {
+    if(this.state.numero <= 9){
+      this.setState({
+        numero: this.state.numero + 1
+      })
+    }
+  } 
+
+  menos = () => {
+      this.setState({
+        numero: this.state.numero - 1
+      })
+  } 
+
+
+  render(){
+    return(
+      <div className='card'>
+        <h1>Contador</h1>
+        <h2>{this.state.numero}</h2>
+        <div className='caixa_btn'>
+          <button className='btn_mns' onClick={this.menos}>-</button>
+          <button className='btn_mais' onClick={this.add}>+</button>
+        </div>
+      </div>
+    )
+  }
+
+
+}
